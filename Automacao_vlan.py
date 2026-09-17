843fc2ae6314eac9652965436d9116bb6233f5f9
alexandretetzner <alexandretetzner@gmail.com>
2026-09-16 23:20:58 -0300

    Correçoes para nova versão do Python
    Correções necessárias para a nova versão do python

--- Automacao_vlan.py
+++ Automacao_vlan.py
@@ -18,13 +18,12 @@ net_connect =ConnectHandler(**Network_Device)
 print()
 print(f'Connected to {Network_Device["host"]}')
 print()
-show_clock()
 print('Fazendo um backup da configuração')
 output_show_runn = net_connect.send_command ('show running-config')
 print()
 
 #configuração da Vlan
-Vlans_padrao = {10:"VLAN_DADOS",20: "VLAN_VOZ",50:"VLAN_SEGURANCA"}
+Vlans_padrao = {10:"VLAN_DADOS",20:"VLAN_VOZ",50:"VLAN_SEGURANCA"}
 for Vlan_ID, Vlan_Name in Vlans_padrao.items():
    Command_vlan = net_connect.send_command('show vlan id ' + str(Vlan_ID),use_textfsm=True)
    if Command_vlan == 'VLAN id ' + str(Vlan_ID) +' not found in current VLAN database':
@@ -35,9 +34,9 @@ for Vlan_ID, Vlan_Name in Vlans_padrao.items():
       print(f'Vlan criada com sucesso')
       #print(output)
    else:
-      Vlan_criada = [item['name'] for item in Command_vlan if item['vlan_id'] == str(Vlan_ID)]
-      if [item['vlan_id'] for item in Command_vlan if item['name'] == str(Vlan_Name)]:
-         print('Vlan já estava criada e com o IP e nome correto')
+      Vlan_criada = [item['vlan_name'] for item in Command_vlan if item['vlan_id'] == str(Vlan_ID)]
+      if [item['vlan_id'] for item in Command_vlan if item['vlan_name'] == str(Vlan_Name)]:
+         print('Vlan já estava criada e com o ID e nome correto')
       else:
          print("A Vlan com ID " +str(Vlan_ID) +" está configurada com o nome " + str(Vlan_criada)+" e está fora do padrã")
 
@@ -46,6 +45,5 @@ print('###### VALIDANDO CONFIGURAÇÕES ######')
 output_show_vlan_brief = net_connect.send_command ('show vlan brief')
 print(output_show_vlan_brief)
 #print(output_Interface_ap)
-show_clock()
 net_connect.disconnect()
 print('Closing Connection')
--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # automacao_de_rede
 Projeto de automação de redes
+
+Versão do Pyton - 3.13.15
+
+---
+Pacotes necessários
+NETMIKO
+$ pip install netmiko
